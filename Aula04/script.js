d82b523101a5f840a42fe391414c4eea2dca423c
Bruno Rossetto <bruno06042001@gmail.com>
2025-04-24 17:20:18 -0300

    Adicionado comentários no js

--- Aula04/script.js
+++ Aula04/script.js
@@ -1,3 +1,3 @@
-window.alert("Minha primeira mensagem!")
-window.confirm("Está gostando de JavaScript?")
-window.prompt("Qual é o seu nome?")
+window.alert("Minha primeira mensagem!") //Vai dar um alerta
+window.confirm("Está gostando de JavaScript?") //Vai aparecer um confirmar na janela
+window.prompt("Qual é o seu nome?") //Vai perguntar seu nome
