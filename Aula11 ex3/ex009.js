8f9565eb8e82ad8de2765ed7d90b26da009eba9b
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-02 14:33:41 -0300

    Criado exercicio de controle de velocidade

--- Aula11 ex3/ex009.js
+++ Aula11 ex3/ex009.js
@@ -1,7 +1,16 @@
-var pais = 'Canada'
-console.log(`Vivendo em ${pais}`)
-if(pais == 'Brasil'){
-    console.log('Você é Brasileiro')
-}else{
-    console.log('Você é Estrangeiro')
+var txtv = document.getElementById('txtvel')
+var res = document.getElementById('res')
+
+function calcular(){
+
+    var vel = Number(txtv.value)
+
+    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}Km/h<strong/></p>`
+
+    if(vel > 60){
+        res.innerHTML += `<p>Você está sendo <strong>MULTADO</strong> por excesso de velocidade!</p>`
+    }else{
+        res.innerHTML += `<p>Você está dentro do limite de velocidade!</p>`
+    }
+    res.innerHTML += `<p>Dirija sempre com cinto de segurança!</p>`
 }
