f76815ab1819aceb4c7989007c0168a90ca83ab2
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-02 14:53:42 -0300

    Criado o identificador de gringo

--- Aula11 ex2/ex008.js
+++ Aula11 ex2/ex008.js
@@ -1,7 +1,20 @@
-var pais = 'Canada'
-console.log(`Vivendo em ${pais}`)
-if(pais == 'Brasil'){
-    console.log('Você é Brasileiro')
-}else{
-    console.log('Você é Estrangeiro')
+var identificador = document.getElementById('identificador')
+var resultado = document.getElementById('resultado')
+
+function verificar(){
+    var pais = identificador.value
+
+    resultado.innerHTML += `<p>Seu país de origem é ${pais}</p>`
+
+    if(pais == 'Brasil'){
+        resultado.innerHTML += `<p>Você é Brasileiro</p>`
+
+    }else{
+        resultado.innerHTML += `<p>Você é gringo</p>`
+
+    }
+
+    resultado.innerHTML += `<p>Pra gringo é mais caro!</p>`
+    
 }
+
