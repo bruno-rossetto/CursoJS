450287ccc3fc255ebe616d8c14d44fb4e478d0dc
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-02 14:18:52 -0300

    Criado condição no ex 8

--- Aula11 ex2/ex008.js
+++ Aula11 ex2/ex008.js
@@ -1,6 +1,7 @@
-var vel = 40
-console.log(`A velocidade do seu carro é ${vel} km/h`)
-if(vel > 60){ //Condição simples
-    console.log('Você ultrapassou a velocidade permitida! MULTADO!')
+var pais = 'Canada'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
 }
-console.log('Dirija sempre usando cinto de segurança!')
