8ca082629135369aac1fdbfe4945f574ba3c8c9e
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-07 18:39:24 -0300

    Feito exercicio de switch

--- Aula12 ex3/ex012.js
+++ Aula12 ex3/ex012.js
@@ -1,10 +1,39 @@
 var agora = new Date()
-var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas.`)
-if(hora < 12){
-    console.log("Bom dia!")
-} else if(hora <= 18){
-    console.log("Boa tarde!")
-} else{
-    console.log("Boa noite!")
+var diaSem = agora.getDay()
+/* 
+    0 = Domingo
+    1 = Segunda
+    2 = Terça
+    3 = Quarta 
+    4 = Quinta
+    5 = Sexta 
+    6 = Sabado
+*/
+
+
+switch(diaSem){
+    case 0: 
+        console.log(`Hoje é Domingo`)
+        break
+    case 1: 
+        console.log(`Hoje é Segunda`)
+        break
+    case 2: 
+        console.log(`Hoje é Terça`)
+        break
+    case 3: 
+        console.log(`Hoje é Quarta`)
+        break
+    case 4: 
+        console.log(`Hoje é Quinta`)
+        break
+    case 5: 
+        console.log(`Hoje é Sexta`)
+        break
+    case 6: 
+        console.log(`Hoje é Sabado`)
+        break
+    default:
+        console.log("[ERRO]Dia inválido")
+        break
 }
