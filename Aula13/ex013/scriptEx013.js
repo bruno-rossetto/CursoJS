e9f0827758872f7854c26b15079ab9a4c2844a29
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-09 10:41:54 -0300

    Arrumado o style

--- Aula13/ex013/scriptEx013.js
+++ Aula13/ex013/scriptEx013.js
@@ -11,10 +11,8 @@ function carregar(){
     } else if(hora >= 12 && hora < 18){
         imagem.src = '/Aula13/ex013/img/fotoTarde.jpg'
         document.body.style.background = '#b9846f'
-
     }else {
         imagem.src = '/Aula13/ex013/img/fotoNoite.jpg'
         document.body.style.background = '#515154'
-
     }
 }
