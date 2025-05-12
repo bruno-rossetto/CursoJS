59c636862bcc55870ef662cee944d21b2cd0e060
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-12 19:02:38 -0300

    Arrumado erro com as imagens

--- Aula13/ex014/scriptex014.js
+++ Aula13/ex014/scriptex014.js
@@ -3,6 +3,7 @@ function verificar(){
     var ano = data.getFullYear()
     var fAno = document.getElementById('ano')
     var resultado = document.getElementById('resultado')
+    var foto = document.getElementById('foto')
 
     if(fAno.value.length == 0 || fAno.value > ano){
         window.alert('[ERROR]Verifique os dados e tente novamente!')
@@ -14,42 +15,43 @@ function verificar(){
         var genero = ''
 
         var imagem = document.createElement('img')
-        imagem.setAttribute('id', 'foto')
+        imagem.setAttribute('id', 'imagem')
 
         if(sexo[0].checked){
             genero = 'Homem'
             if(idade >= 0 && idade < 10){
                 //criança
-                imagem.setAttribute('src', 'Aula13\ex014\img\criancaM.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/criancaM.jpg')
             }else if(idade >= 10 && idade < 18){
                 //adolecente
-                imagem.setAttribute('src', 'Aula13/ex014/img/garoto.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/garoto.jpg')
             }else if(idade >= 18 && idade < 60){
                 //adulto
-                imagem.setAttribute('src', 'Aula13\ex014\img\homem.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/homem.jpg')
             }else{
                 //idoso
-                imagem.setAttribute('src', 'Aula13\ex014\img\idoso.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/idoso.jpg')
             }
         } else if(sexo[1].checked){
             genero = 'Mulher'
             if(idade >= 0 && idade < 10){
                 //criança
-                imagem.setAttribute('src', 'Aula13\ex014\img\criancaF.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/criancaF.jpg')
             }else if(idade >= 10 && idade < 18){
                 //adolecente
-                imagem.setAttribute('src', 'Aula13\ex014\img\garota.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/garota.jpg')
             }else if(idade >= 18 && idade < 60){
                 //adulto
-                imagem.setAttribute('src', 'Aula13\ex014\img\mulher.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/mulher.jpg')
             }else{
                 //idoso
-                imagem.setAttribute('src', 'Aula13\ex014\img\idosa.jpg')
+                imagem.setAttribute('src', '/Aula13/ex014/img/idosa.jpg')
             }
         }
         resultado.style.textAlign = 'center'
         resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        resultado.appendChild(imagem)
+        resultado.appendChild(foto)
+        foto.appendChild(imagem)
     }
 
 }
