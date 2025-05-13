37e78f9db934c6904205611c3481f4c39f7fcad2
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-13 18:47:05 -0300

    Criado exemplo de do while

--- Aula13/script.js
+++ Aula13/script.js
@@ -1,7 +1,16 @@
 var contador = 1
 
+do {
+
+    console.log(`Passo ${contador}`)
+    contador++ //contador = contador + 1
+} while(contador <= 6)
+
+
+/*
 while(contador <= 6){
 
     console.log(`Passo ${contador}`)
     contador++ //contador = contador + 1
 }
+ */ 
