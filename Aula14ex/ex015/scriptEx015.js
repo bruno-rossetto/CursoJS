959fa604b6239cc9bc05e05a6160895f82818ddf
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-16 19:39:05 -0300

    Por hoje é isso, amanhã arrumo

--- Aula14ex/ex015/scriptEx015.js
+++ Aula14ex/ex015/scriptEx015.js
@@ -3,13 +3,15 @@ function contar(){
     var fim = document.getElementById('fimContador')
     var passo = document.getElementById('passoContador')
     var mensagemResultado = document.getElementById('resultado')
-    var resultado = 0 
+    var resultado = ''
     
     
-    
+    if(){
+
     for(var numero = inicio; numero <= fim; numero += passo){
         resultado += numero
     }
     console.log(resultado)
     return resultado
+    }
 }
