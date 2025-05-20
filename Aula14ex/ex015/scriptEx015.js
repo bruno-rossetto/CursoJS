9b62c908ef0e832b2d8f37b1f24789360b9733ca
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-20 14:57:44 -0300

    Finalizado o exercicio

--- Aula14ex/ex015/scriptEx015.js
+++ Aula14ex/ex015/scriptEx015.js
@@ -1,17 +1,33 @@
 function contar(){
-    var inicio = document.getElementById('inicioContador')
-    var fim = document.getElementById('fimContador')
-    var passo = document.getElementById('passoContador')
-    var mensagemResultado = document.getElementById('resultado')
-    var resultado = ''
+    let inicio = document.getElementById('inicioContador')
+    let fim = document.getElementById('fimContador')
+    let passo = document.getElementById('passoContador')
+    let resultado = document.getElementById('resultado')
     
-    
-    if(){
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
+        resultado.innerHTML = "Impossível contar!"
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        resultado.innerHTML = `Contando: `
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if(i < f){
+            // Contagem crescente
+            for(let c = i; c <= f; c += p){
+            resultado.innerHTML += ` ${c} `
+            }
+        }else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                resultado.innerHTML += ` ${c} `
+            }
+        }
 
-    for(var numero = inicio; numero <= fim; numero += passo){
-        resultado += numero
-    }
-    console.log(resultado)
-    return resultado
+        
     }
 }
