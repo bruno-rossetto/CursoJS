1098ee265ec96f10025483ae848ca126e60c1b55
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-30 18:33:12 -0300

    terminado exercício da tabuada

--- Aula14ex/ex016/scriptex016.js
+++ Aula14ex/ex016/scriptex016.js
@@ -6,6 +6,16 @@ function geradorTabuada(){
         window.alert("Por favor, digite um número!")
     }else {
         let n = Number(numero.value)
+        let c = 1
+
+        tabuada.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tabuada.appendChild(item)
+            c++
+        }
     }
 
 
