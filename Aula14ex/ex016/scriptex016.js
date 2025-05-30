7c0f3e39167e77dbcbe3dd8f2c98248685e41f49
Bruno Rossetto <bruno06042001@gmail.com>
2025-05-30 18:24:51 -0300

    iniciada função de gerar tabuada

--- Aula14ex/ex016/scriptex016.js
+++ Aula14ex/ex016/scriptex016.js
@@ -1,57 +1,13 @@
-function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fAno = document.getElementById('ano')
-    var resultado = document.getElementById('resultado')
-    var foto = document.getElementById('foto')
+function geradorTabuada(){
+    let numero = document.getElementById('numero')
+    let tabuada = document.getElementById('seltab')
 
-    if(fAno.value.length == 0 || fAno.value > ano){
-        window.alert('[ERROR]Verifique os dados e tente novamente!')
-    }else{
-        var sexo = document.getElementsByName('sexo')
-
-        var idade = ano - Number(fAno.value)
-
-        var genero = ''
-
-        var imagem = document.createElement('img')
-        imagem.setAttribute('id', 'imagem')
-
-        if(sexo[0].checked){
-            genero = 'Homem'
-            if(idade >= 0 && idade < 10){
-                //criança
-                imagem.setAttribute('src', '/Aula12/ex014/img/criancaM.jpg')
-            }else if(idade >= 10 && idade < 18){
-                //adolecente
-                imagem.setAttribute('src', '/Aula12/ex014/img/garoto.jpg')
-            }else if(idade >= 18 && idade < 60){
-                //adulto
-                imagem.setAttribute('src', '/Aula12/ex014/img/homem.jpg')
-            }else{
-                //idoso
-                imagem.setAttribute('src', '/Aula12/ex014/img/idoso.jpg')
-            }
-        } else if(sexo[1].checked){
-            genero = 'Mulher'
-            if(idade >= 0 && idade < 10){
-                //criança
-                imagem.setAttribute('src', '/Aula12/ex014/img/criancaF.jpg')
-            }else if(idade >= 10 && idade < 18){
-                //adolecente
-                imagem.setAttribute('src', '/Aula12/ex014/img/garota.jpg')
-            }else if(idade >= 18 && idade < 60){
-                //adulto
-                imagem.setAttribute('src', '/Aula12/ex014/img/mulher.jpg')
-            }else{
-                //idoso
-                imagem.setAttribute('src', '/Aula12/ex014/img/idosa.jpg')
-            }
-        }
-        resultado.style.textAlign = 'center'
-        resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        resultado.appendChild(foto)
-        foto.appendChild(imagem)
+    if(numero.value.length == 0){
+        window.alert("Por favor, digite um número!")
+    }else {
+        let n = Number(numero.value)
     }
+
+
 
 }
