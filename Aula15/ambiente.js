ae49be9155ee2b4d5cf49f18b4d6523447d1d850
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-01 16:24:39 -0300

    Add forma de pegar posição especificado array

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -2,10 +2,12 @@ let num = [5,8,2,9,3]
 
 num[5] = 6 //Adição no array sabendo a casa final
 
-num.push(10) //Adição no array sem se importar em saber a casa final
+num.push(1) //Adição no array sem se importar em saber a casa final
 
 num.sort()//Alinha em ordem crescente o array
 
-console.log(`Nosso vetor é o ${num}`)
+console.log(`Nosso vetor é o ${num}.`)
 
 console.log(`Nosso vetor tem ${num.length} posições.`)//Para saber o tamanho do array
+
+console.log(`O primeiro valor do vetor é ${num[0]}`)//Para pegar alguma posição especifica do array
