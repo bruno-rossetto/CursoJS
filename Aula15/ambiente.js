9dd8700a5e4ecbe17854c8353b3d5ef2a2fbf2ad
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-01 16:21:10 -0300

    Mudança no num.length

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -4,9 +4,8 @@ num[5] = 6 //Adição no array sabendo a casa final
 
 num.push(10) //Adição no array sem se importar em saber a casa final
 
-num.length//Para saber o tamanho do array
-
 num.sort()//Alinha em ordem crescente o array
 
-
 console.log(`Nosso vetor é o ${num}`)
+
+console.log(`Nosso vetor tem ${num.length} posições.`)//Para saber o tamanho do array
