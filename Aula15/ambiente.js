1ec5b71b2ae4f231a26223b4e465ba7480f92458
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-01 16:30:57 -0300

    Criado vetor na tela

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -11,3 +11,4 @@ console.log(`Nosso vetor é o ${num}.`)
 console.log(`Nosso vetor tem ${num.length} posições.`)//Para saber o tamanho do array
 
 console.log(`O primeiro valor do vetor é ${num[0]}`)//Para pegar alguma posição especifica do array
+
