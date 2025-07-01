d4f102644761bb9a67c33b03308c146dc4373a98
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-01 16:19:16 -0300

    Começo da aula sobre array

---
+++ Aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5,8,2,9,3]
+
+num[5] = 6 //Adição no array sabendo a casa final
+
+num.push(10) //Adição no array sem se importar em saber a casa final
+
+num.length//Para saber o tamanho do array
+
+num.sort()//Alinha em ordem crescente o array
+
+
+console.log(`Nosso vetor é o ${num}`)
