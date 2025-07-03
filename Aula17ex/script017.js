87ce66d008fb9a5ef5f0b791b4561aee77a6258c
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-03 17:18:48 -0300

    finalizando

--- Aula17ex/script017.js
+++ Aula17ex/script017.js
@@ -27,6 +27,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${numero.value} adicionado`
         lista.appendChild(item)
+        resultado.innerHTML = ''
     }else{
         window.alert('Valor inválido ou já adicionado a lista.')
     }
@@ -39,9 +40,28 @@ function finalizar(){
         window.alert('Adiciones valores para finalizar.')
     } else{
         let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma= 0
+        let media = 0
+
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / total
 
         resultado.innerHTML = ''
-        resultado.innerHTML += `<p>Ao todo, temos ${total} nùmeros cadastrados.</p>`
+        resultado.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        resultado.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        resultado.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        resultado.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`
+        resultado.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
         
+
+
     }
 }
