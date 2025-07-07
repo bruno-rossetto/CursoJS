9a58672c0a13c54e2d97f617e115a05671cdc422
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-07 15:18:58 -0300

    ultima aula de js

---
+++ Aula18/objeto.js
@@ -0,0 +1,11 @@
+let amigo = {nome: 'Jose', sexo: 'M', peso: 85, engordar(p){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+
+console.log(amigo)
+
+console.log(`${amigo.nome} pesa ${amigo.peso}`)
+
+amigo.engordar(0)
