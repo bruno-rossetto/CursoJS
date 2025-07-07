a0c11557b71c6ed6fa42f9cd9f695359f4423e71
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-07 17:19:07 -0300

    Ajeitei uma coisinha

--- treinoJs/treinoJs.js
+++ treinoJs/treinoJs.js
@@ -1,4 +1,3 @@
-
 function enviar(){
     let nome = document.getElementById('inputNome').value
     let idade = document.getElementById('inputIdade').value
@@ -6,7 +5,7 @@ function enviar(){
     if(nome === '' || idade === ''){
         window.alert('Por favor, preencher todos os campos!')
     }
-    let resultado = `Olá ${nome}, tudo bem? Você tem ${idade}.`
+    let resultado = `Olá ${nome}, tudo bem? Você tem ${idade} anos.`
 
     document.getElementById('resultado').textContent = resultado
     nome.value = ''
