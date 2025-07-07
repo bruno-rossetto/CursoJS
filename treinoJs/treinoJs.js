41344bcb9cd50dc3c963f6977c65a1de10970cf7
Bruno Rossetto <bruno06042001@gmail.com>
2025-07-07 17:16:58 -0300

    Criada função para entregar o nome e a idade da pessoa

---
+++ treinoJs/treinoJs.js
@@ -0,0 +1,15 @@
+
+function enviar(){
+    let nome = document.getElementById('inputNome').value
+    let idade = document.getElementById('inputIdade').value
+
+    if(nome === '' || idade === ''){
+        window.alert('Por favor, preencher todos os campos!')
+    }
+    let resultado = `Olá ${nome}, tudo bem? Você tem ${idade}.`
+
+    document.getElementById('resultado').textContent = resultado
+    nome.value = ''
+    idade.value = ''
+    nome.focus()
+}
